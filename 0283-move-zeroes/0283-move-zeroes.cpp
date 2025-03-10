ad7a92a3e9a90a1e5eb92ad316a27c657f8ae787
ritikrawat0408 <rwt.ritik@gmail.com>
2025-03-10 23:16:07 +0530

    Time: 2 ms (19.99%), Space: 23.9 MB (17.94%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> result(n);
-        int index = 0;
-        
-        //storing the non zero elmnts in result array
-        for(int i = 0;i < n;++i){
-            if(nums[i] != 0){
-                result[index] = nums[i];
-                index++;
+        int lastnonzerofound=0;
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[lastnonzerofound],nums[i]);
+                lastnonzerofound++;
             }
         }
-        //filling the leftover indexes in result with 0
-        for(int i = index;i < n;++i){
-            result[i] = 0;
-        }
-        //copy the result array to the original array
-        for(int i =0 ; i < n;++i){
-            nums[i] = result[i];
-        }
-        
     }
 };
