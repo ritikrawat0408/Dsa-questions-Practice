fcbb95761b1cdfabc85eb875c9a9456345eb08ee
ritikrawat0408 <rwt.ritik@gmail.com>
2025-03-20 03:13:16 +0530

    Time: 0 ms (100%), Space: 27.9 MB (97.9%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n = nums.size();
-
-        for (int i = 0; i < n; i++) {
-            int count = 1;  // Count the current element itself
-            for (int j = i + 1; j < n; j++) {  // Start checking from the next element
-                if (nums[j] == nums[i]) {
-                    count++;
-                }
-            }
-            if (count > n / 2) {
-                return nums[i];
-            }
-        }
-        return -1;  // This case will never happen since the problem guarantees a majority element.
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()/2];
     }
 };
